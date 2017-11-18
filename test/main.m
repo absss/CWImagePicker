4a1a94db72756f7700560a98eb4e993dda30491a
yuanhongyang <yuanhongyang@oaserver.dw.gdbbk.com>
2017-11-18 15:03:16 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by app on 2017/11/18.
+//  Copyright © 2017年 app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
